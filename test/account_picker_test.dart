d521fbcfa310c28c36daaaceb1896f151811748b
Tolotra <samuel.randriakotonjanahary@mcb.mu>
2021-05-24 02:27:55 +0400

    Null safety support

--- test/account_picker_test.dart
+++ test/account_picker_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:account_picker/account_picker.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('account_picker');
@@ -16,5 +15,4 @@ void main() {
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
-
 }
--- test/accountpicker_test.dart
+++ test/accountpicker_test.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
+import 'package:account_picker/account_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:account_picker/account_picker.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('account_picker');
@@ -25,7 +27,8 @@ void main() {
   });
 
   test('emailHint', () async {
-    final EmailResult emailResult = await (AccountPicker.emailHint() as FutureOr<EmailResult>);
+    final EmailResult emailResult =
+        await (AccountPicker.emailHint() as FutureOr<EmailResult>);
     expect(emailResult.email, 'foo@example.com');
     expect(emailResult.type, 'google');
   });
